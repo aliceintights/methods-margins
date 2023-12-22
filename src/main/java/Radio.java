e81f88257dd18165c0cf32b2224005722b169fd2
irina <obesyuk@gmail.com>
2023-12-23 01:49:38 +0300

    radio class added

---
+++ src/main/java/Radio.java
@@ -0,0 +1,25 @@
+public class Radio {
+    private int radio;
+    private int volume;
+
+    public Radio(int radio, int volume) {
+        this.radio = radio;
+        this.volume = volume;
+    }
+
+    public int getRadio() {
+        return radio;
+    }
+
+    public void setRadio(int radio) {
+        this.radio = radio;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+}
