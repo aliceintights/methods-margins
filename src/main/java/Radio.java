354d433a1132954d6f3047ea84aa77720860348d
irina <obesyuk@gmail.com>
2024-01-02 20:07:59 +0300

    added to radio class

--- src/main/java/Radio.java
+++ src/main/java/Radio.java
@@ -2,6 +2,10 @@ public class Radio {
     private int radio;
     private int volume;
     private int radioStationNumber;
+    private int next;
+    private int prev;
+    private int volumeHigher;
+    private int volumeLower;
 
     public int getRadio() {
         return radio;
@@ -25,5 +29,37 @@ public class Radio {
 
     public void setRadioStationNumber(int radioStationNumber) {
         this.radioStationNumber = radioStationNumber;
+    }
+
+    public int getNext() {
+        return next;
+    }
+
+    public void setNext(int next) {
+        this.next = next;
+    }
+
+    public int getPrev() {
+        return prev;
+    }
+
+    public void setPrev(int prev) {
+        this.prev = prev;
+    }
+
+    public int getVolumeHigher() {
+        return volumeHigher;
+    }
+
+    public void setVolumeHigher(int volumeHigher) {
+        this.volumeHigher = volumeHigher;
+    }
+
+    public int getVolumeLower() {
+        return volumeLower;
+    }
+
+    public void setVolumeLower(int volumeLower) {
+        this.volumeLower = volumeLower;
     }
 }
